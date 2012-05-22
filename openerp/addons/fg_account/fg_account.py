b6e93d591faf8fcf14429b3c4054bf8d005f7308
Daniel Yang <daniel.yang.zhenyu@gmail.com>
2012-05-22 13:33:35 +0800

    sad

--- openerp/addons/fg_account/fg_account.py
+++ openerp/addons/fg_account/fg_account.py
@@ -5,12 +5,35 @@ from osv import fields
 import time
 
 
-class fg_account_journey(osv.osv):
-    _name = 'fg_account.journey'
-    _description = '账本'
+class sale_order(osv.osv):
+    _inherit = 'fg_sale.order'
+    _columns = {
+        'bill_line': fields.one2many('fg_account.rbill.line', 'rbill_id','收款明细', readonly=True),
+    }
+
+class fg_account_rbill_line(osv.osv):
+    _name = 'fg_account.rbill.line'
+    _description = '收款单明细'
     
     _columns = {
-        'name': fields.char('名称', size=40, required=True),
-        'factor':fields.float('换算率', digits=(8, 1)),
+        'name':fields.related('rbill_id', 'name', type='char', string='单号'),
+        'rbill_id': fields.many2one('fg_account.rbill', '收款单', required=True, ondelete='cascade', select=True),
+        'order_id': fields.many2one('fg_sale.order', '订单', required=True, ondelete='cascade', select=True),
     }
+
+class fg_account_rbill(osv.osv):
+    _name = 'fg_account.rbill'
+    _description = '收款单'
+    
+    _columns = {
+        'name': fields.char('单号', size=64, select=True, readonly=True),
+        'date_issue': fields.date('收款日期', required=True, readonly=True, select=True, states={'draft': [('readonly', False)]}),
+        'date_confirm': fields.date('审核日期', readonly=True, select=True),
+        'user_id': fields.many2one('res.users', '制单人', select=True, readonly=True),
+        'confirmer_id': fields.many2one('res.users', '审核人', select=True, readonly=True),
+        'bill_line': fields.one2many('fg_account.rbill.line', 'rbill_id', '收款单', readonly=True, states={'draft': [('readonly', False)]}),
+        'partner_id': fields.many2one('res.partner', '客户', readonly=True, states={'draft': [('readonly', False)]}, required=True, change_default=True, select=True), 
+    }
+    
+
 
