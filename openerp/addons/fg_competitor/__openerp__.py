6ed76aa169827190dd076ec7561d8c58d4103889
杨振宇 <>
2012-12-06 20:49:54 +0800

    update to dev

--- openerp/addons/fg_competitor/__openerp__.py
+++ openerp/addons/fg_competitor/__openerp__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- encoding: utf-8 -*-
 
 {
@@ -20,27 +19,4 @@
     'active': False,
     'application':True,
     'css':['static/src/css/competitor.css']
-=======
-# -*- encoding: utf-8 -*-
-
-{
-    'name': '竞争对手分析',
-    'version': '1.0',
-    'category' : '富光',
-    'description': """富光竞争对手分析子系统""",
-    'author': '杨振宇',
-    'website': 'http://www.fuguang.cn',
-    'depends': ['base', 'board'],
-    'init_xml': [],
-    'update_xml': [
-        'security/groups.xml',
-        'security/ir.model.access.csv',
-        'competitor.xml',
-    ],
-    'demo_xml': [],
-    'installable': True,
-    'active': False,
-    'application':True,
-    'css':['static/src/css/competitor.css']
->>>>>>> 114326c9601ad8c13a1884a69ead2e8c1cf432d1
 }
