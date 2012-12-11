b69ea087b20980078a49424edd0ded3b469d705f
Daniel Yang <daniel.yang.zhenyu@gmail.com>
2012-12-11 16:44:50 +0800

    add report.

--- openerp/addons/fg_hr/fg_hr.py
+++ openerp/addons/fg_hr/fg_hr.py
@@ -22,20 +22,17 @@ class fghr_attendance(osv.osv):
     _columns = {
         'employee_id': fields.many2one('hr.employee', '员工', required=True, select=True),
         'date': fields.date('考勤日期', required=True, select=True),
-        'date_import': fields.date('导入日期'),
-        'am_checkin': fields.datetime('早晨上班'),
-        'am_checkout': fields.datetime('早晨下班'),
-        'pm_checkin': fields.datetime('下午上班'),
-        'pm_checkout': fields.datetime('下午下班'),
-        'late': fields.integer('迟到(分钟)'),
-        'early': fields.integer('早退(分钟)'),
-        'state': fields.selection([('normal', '正常'), ('late', '迟到'), ('early', '早退'), ('abnormal','记录不完整')], '状态', select=True),
-        'note':fields.char('附注', size=200),
+        'attendance': fields.integer('应出勤'),
+        'checkin': fields.integer('实际出勤'),
+        'skipping': fields.integer('旷工'),
+        'late': fields.integer('迟到(次)'),
+        'late_min': fields.integer('迟到(分钟)'),
+        'early': fields.integer('早退(次)'),
+        'early_min': fields.integer('早退(分钟)'),
     }
     
     _order = 'date desc'
     
     _defaults = {
-        'date_import': fields.date.context_today,
-        'state': 'normal',
+        'date': fields.date.context_today,
     }
--- openerp/addons/fg_hr/wizard/wizards.py
+++ openerp/addons/fg_hr/wizard/wizards.py
@@ -12,17 +12,16 @@ class attendance_import(osv.osv_memory):
     _description = "导入考勤明细"
     
     _columns = {
-        'work_time': fields.selection([('summer', '夏季'), ('winter', '冬季')], '时令'),
+        'date': fields.date('日期',  required=True),
         'excel': fields.binary('excel文件', filters='*.xls'),
     }
     
     _defaults = {
-        'work_time': 'winter',
+        'date': fields.date.context_today,
     }
     
     def import_list(self, cr, uid, ids, context=None):
         result = {'type': 'ir.actions.act_window_close'}
-        
         
         for wiz in self.browse(cr,uid,ids):
             if not wiz.excel: continue
@@ -32,73 +31,31 @@ class attendance_import(osv.osv_memory):
             att_obj = self.pool.get('fg_hr.attendance')
             act_obj = self.pool.get('ir.actions.act_window')
             mod_obj = self.pool.get('ir.model.data')
-            partner_obj = self.pool.get('res.partner')
+
             empl_obj = self.pool.get('hr.employee')
             
             new_ids = []
             for rx in range(sh.nrows):
-                data = {}
-                empl_name = sh.cell(rx, 2).value.strip()
+                data = {'date': wiz.date}
+                
+                empl_name = sh.cell(rx, 3).value.strip()
                 if not empl_name or empl_name=='姓名' : continue
                 empl_list = empl_obj.search(cr, uid, [('name','=',empl_name)])
                 if empl_list:
                     data['employee_id'] = empl_list[0]
                 else:
                     continue
-
+                
                 try:
-                    date_s = sh.cell(rx, 4).value.strip()
-                    date = time.strptime(date_s,'%Y-%m-%d')
-                    data['date'] = date_s
-                    am_s = am_e = pm_s = pm_e = ''
-                    if sh.cell(rx, 5).value:
-                        am_s = xlrd.xldate_as_tuple(sh.cell(rx, 5).value, 0)
-                        am_s = convert_datetime(date, am_s)
-                        data['am_checkin'] = datetime.strftime(am_s-timedelta(hours=8), '%Y-%m-%d %H:%M:%S')
-                        
-                    if sh.cell(rx, 6).value:
-                        am_e = xlrd.xldate_as_tuple(sh.cell(rx, 6).value, 0)
-                        am_e = convert_datetime(date, am_e)
-                        data['am_checkout'] = datetime.strftime(am_e-timedelta(hours=8), '%Y-%m-%d %H:%M:%S')
-                    
-                    if sh.cell(rx, 7).value:
-                        pm_s = xlrd.xldate_as_tuple(sh.cell(rx, 7).value, 0)
-                        pm_s = convert_datetime(date, pm_s)
-                        data['pm_checkin'] = datetime.strftime(pm_s-timedelta(hours=8), '%Y-%m-%d %H:%M:%S')
-                        
-                    if sh.cell(rx, 8).value:
-                        pm_e = xlrd.xldate_as_tuple(sh.cell(rx, 8).value, 0)
-                        pm_e = convert_datetime(date, pm_e)
-                        data['pm_checkout'] = datetime.strftime(pm_e-timedelta(hours=8), '%Y-%m-%d %H:%M:%S')
-
-                        am_start_work = datetime.strptime(' '.join([date_s, '07:30:00']), '%Y-%m-%d %H:%M:%S')
-                        am_end_work = datetime.strptime(' '.join([date_s, '11:30:00']), '%Y-%m-%d %H:%M:%S')
-                        pm_start_work = datetime.strptime(' '.join([date_s, '13:30:00']), '%Y-%m-%d %H:%M:%S')
-                        pm_end_work = datetime.strptime(' '.join([date_s, '17:30:00']), '%Y-%m-%d %H:%M:%S')
-                        
-                        if wiz.work_time == 'summer':
-                            am_start_work = datetime.strptime(' '.join([date_s, '07:00:00']), '%Y-%m-%d %H:%M:%S')
-                            pm_start_work = datetime.strptime(' '.join([date_s, '14:30:00']), '%Y-%m-%d %H:%M:%S')
-                            pm_end_work = datetime.strptime(' '.join([date_s, '18:00:00']), '%Y-%m-%d %H:%M:%S')
-                    
-                    if am_s and am_e and pm_s and pm_e:
-                        #see if it's late.
-                        data['late'] = 0
-                        am_dd = (am_s-am_start_work).total_seconds()
-                        if am_dd > 60:
-                            data['state'] = 'late'
-                            data['late'] = round(am_dd/60)
-                        
-                        pm_dd = (pm_s-pm_start_work).total_seconds()
-                        if pm_dd > 60:
-                            data['state'] = 'late'
-                            data['late'] = data['late'] + round(pm_dd/60)
-                        
-                        if (am_e < am_end_work) or (pm_e<pm_end_work):
-                            data['state'] = 'early'
-                    else:
-                        data['state'] = 'abnormal'
-                except Exception as e:
+                    data['attendance'] = sh.cell(rx, 4).value
+                    data['checkin'] = sh.cell(rx, 5).value
+                    data['skipping'] = sh.cell(rx, 7).value
+                    data['late'] = sh.cell(rx, 8).value
+                    data['late_min'] = sh.cell(rx, 9).value
+                    data['early'] = sh.cell(rx, 10).value
+                    data['early_min'] = sh.cell(rx, 11).value
+                except Exception as err:
+                    print err
                     continue
 
                 id = att_obj.create(cr, uid, data)
@@ -117,9 +74,7 @@ class salary_export(osv.osv_memory):
     _description = "导出"
     
     _columns = {
-        'work_time': fields.selection([('summer', '夏季'), ('winter', '冬季')], '时令'),
-        'date_start': fields.date('开始日期',  required=True),
-        'date_end': fields.date('结束日期',  required=True),
+        'date': fields.date('考勤开始日期',  required=True),
         'name': fields.char('文件名', 16,),
         'data': fields.binary('文件',),
         'state': fields.selection( [('choose','choose'),   # choose
@@ -128,11 +83,10 @@ class salary_export(osv.osv_memory):
     }
     
     _defaults = {
-        'date_end': fields.date.context_today,
+        'date': fields.date.context_today,
         'state': lambda *a: 'choose',
         'name': 'salary.xls',
     }
-    
     
     def export_excel(self, cr, uid, ids, context=None):
         book = xlwt.Workbook(encoding='utf-8')
@@ -140,57 +94,59 @@ class salary_export(osv.osv_memory):
         
         sheet1 = book.add_sheet('salary')
         sql = """
-        SELECT
-        	att.employee_id,
-        	SUM(
-        		CASE
-        		WHEN att."state" = 'normal' THEN
-        			1
-        		ELSE
-        			0
-        		END
-        	)AS normal,
-        	SUM(
-        		CASE
-        		WHEN att."state" = 'late' THEN
-        			att.late
-        		ELSE
-        			0
-        		END
-        	)AS late,
-        	SUM(
-        		CASE
-        		WHEN att."state" = 'early' THEN
-        			1
-        		ELSE
-        			0
-        		END
-        	)AS early,
-        	SUM(
-        		CASE
-        		WHEN att."state" = 'abnormal' THEN
-        			1
-        		ELSE
-        			0
-        		END
-        	)AS abnormal
-        FROM
-        	fg_hr_attendance att
-        WHERE
-        	att."date" >= to_date('2012-11-01', 'YYYY-MM-DD')
-        AND att."date" <= to_date('2012-11-30', 'YYYY-MM-DD')
-        GROUP BY
-        	att.employee_id
+            SELECT
+                    res."name",
+                    att.attendance,
+                    att.checkin,
+                    att.late,
+                    att.late_min,
+                    att.early,
+                    att.early_min,
+                    salary.basic,
+                    salary.post,
+                    salary.performance,
+                    round(
+                            (salary.basic + salary.post) / att.attendance * att.checkin - att.late_min * 5 + salary.performance
+                    ) AS salary
+            FROM
+                    fg_hr_attendance att
+            JOIN fg_hr_salary salary ON att.employee_id = salary.employee_id
+            JOIN hr_employee empl ON empl."id" = att.employee_id
+            JOIN resource_resource res ON res."id" = empl.resource_id
+            WHERE
+                    EXTRACT (YEAR FROM att."date") = %s
+            AND EXTRACT (MONTH FROM att."date") = %s
         """
+        m = this.date.split('-')[1]
+        y = this.date.split('-')[0]
         
-        cr.execute()
+        cr.execute(sql % (y, m))
         
+        sheet1.write(0, 0, '员工')
+        sheet1.write(0, 1, '应出勤')
+        sheet1.write(0, 2, '实际出勤')
+        sheet1.write(0, 3, '迟到(次数)')
+        sheet1.write(0, 4, '迟到(分钟)')
+        sheet1.write(0, 5, '早退(次数)')
+        sheet1.write(0, 6, '早退(分钟)')
+        sheet1.write(0, 7, '基本工资')
+        sheet1.write(0, 8, '岗位工资')
+        sheet1.write(0, 9, '绩效')
+        sheet1.write(0, 10, '社保')
+        sheet1.write(0, 11, '工资')
+        
+        row = 1
+        
+        for p in cr.fetchall():
+            col = 0
+            for n in p:
+                sheet1.write(row, col, n)
+                col = col + 1
+            row = row + 1
+            
         buf=cStringIO.StringIO()
         book.save(buf)
 
         out=base64.encodestring(buf.getvalue())
 
         return self.write(cr, uid, ids, {'state':'get', 'data':out, 'name':this.name }, context=context)
-        
-            
-    
